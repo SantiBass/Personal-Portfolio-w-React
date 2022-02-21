67f40691dbcbde130831bbe100c10632219924b3
SantiBass <sibarraryan@gmail.com>
2022-02-21 15:38:28 -0800

    working on the header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
 function App() {
   return (
-    <div>App</div>
+    <div>App
+        <Header></Header>
+        <Footer></Footer>
+    </div>
   )
 }
 
--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -2,12 +2,10 @@ import React from 'react'
 import macLogo from '../../Assets/images/macLogo.jpg'
 function Navbar() {
   return (
-    <div className='navbar'>Thin is the Navbar
-    <div className='leftSide'>
-        <img src={macLogo} alt= "logo"/>
-    </div>
-    <div className='rightSide'></div>
-    </div>
+   
+    
+    <div>Navbar</div>
+  
   )
 }
 
