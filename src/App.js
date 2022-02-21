7c036cd8c448e35ba964d3118a5046724fe394b6
SantiBass <sibarraryan@gmail.com>
2022-02-21 15:28:29 -0800

    reorganizing files layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-  // import cover from './Assets/images/nick-karvounis-3_ZGrsirryY-unsplash.jpg';
-  // everything runs from this file
-import './App.css';
-import Navbar from './components/navbar/Navbar';
-import Home from './pages/Home';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-// import About from './components/about/About';
+import React from 'react'
+
 function App() {
   return (
-  <div className="App"><h1>Main</h1>
-    <Router>
-      <Navbar/>
-        <Routes>
-      <Route path="/" exact component={Home}/>
-    </Routes>
-   </Router>
-
-
-  </div>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
