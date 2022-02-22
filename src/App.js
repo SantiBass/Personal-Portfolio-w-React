9ed029f635692bceb0827f0de32f1278e88e2b1a
SantiBass <sibarraryan@gmail.com>
2022-02-21 19:43:48 -0800

    working on navbarrja

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-  // import cover from './Assets/images/nick-karvounis-3_ZGrsirryY-unsplash.jpg';
   // everything runs from this file
-import './App.css';
-// import Navbar from './Navbar';
-
-
-// import About from './components/about/About';
+  // import './App.css';
+  // import Navbar from './Navbar';
+  // import About from './components/about/About';
+  
+  import React from 'react';
+  import Header from './Header';
+  import Footer from './Footer';
 function App() {
   return (
  
-<div><h1>Main</h1></div>
-  
-  )
+<div><h1>Main</h1>
+  <Header/>
+  <Footer/>
+ </div> 
+ );
 }
 
 export default App;
